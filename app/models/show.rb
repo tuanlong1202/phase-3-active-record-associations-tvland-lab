1eb2bd659af43a40fc702e04b8549ffa7d95f20e
tuanlong1202 <tuanlong1202@gmail.com>
2021-08-14 23:35:01 -0400

    Phase 3 active record associations tvland lab

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors ,through: :characters
+  belongs_to :network
   
+  def actors_list
+    arrayActorsList = []
+    self.actors.each do |actor|
+        arrayActorsList << "#{actor.first_name} #{actor.last_name}"
+    end
+    arrayActorsList
+  end
+
 end
